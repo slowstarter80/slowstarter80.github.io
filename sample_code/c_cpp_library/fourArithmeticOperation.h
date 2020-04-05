f7cceee0178afda67aa86f4621f3fee624d85bc0
chuljeon39a <jchsonez@daum.net>
2020-04-05 23:21:00 +0900

    sample code update(c/cpp library)

---
+++ sample_code/c_cpp_library/fourArithmeticOperation.h
@@ -0,0 +1,13 @@
+#ifndef __FOURARITHMETICOPERATION_
+#define __FOURARITHMETICOPERATION_
+
+template <typename T>
+class fourArithmeticOperation {
+	public :
+		T addition(T a, T b);
+		T subtraction(T a, T b);
+		T multiplication(T a, T b);
+		T division(T a, T b);
+};
+
+#endif
